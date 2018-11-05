b4a4ca7d308616f8f895b46b42423cd7280d0d57
d5nguyen <d5nguyen@gmail.com>
2018-11-05 00:03:29 -0500

    scrolling effects

--- resources/js/script.js
+++ resources/js/script.js
@@ -19,8 +19,25 @@ $(document).ready(function() {
     $('.js--scroll-to-start').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
-        
+    /* $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
+    });    
 
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
+    });
+    
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
+    });
+    
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
+    });
+    
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
+    });
     /* Navigation scroll */
     
     $(function() {
